9d7a80b7c62552437ac2347cc8be7df4a5777542
João Pedro <jqueirozleite@gmail.com>
2026-05-13 22:55:34 -0300

    Add files via upload

---
+++ FATEC-PEM-20261/Atividades/main.c
@@ -0,0 +1,20 @@
+/*-------------------------------------------------------*
+*                      FATEC Ipiranga                    *
+* Disciplina: Programaçao Estruturada e Modular          *
+*          Prof. Veríssimo                               *
+*--------------------------------------------------------*
+*Objetivo: Criação de bibliotecas personalizadas         *
+aplicando recursividade e modularização                  *
+* Data - 13/05/2026                                      * 
+* Autor: João Pedro de Queiroz Leite   2040482522003     *
+*--------------------------------------------------------*/
+
+#include "interface.h"
+
+// O Main contém apenas a chamada da função controladora
+int main() {
+    
+    interface();
+    
+    return 0;
+}
